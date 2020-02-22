bae62c6a27297c2671038b2b5cbe438bc751cb07
earacena <30157858+earacena@users.noreply.github.com>
2020-02-22 16:32:59 -0500

    Update cpu.hpp

--- cpu.hpp
+++ cpu.hpp
@@ -1,5 +1,5 @@
 /*
- * Author:                  Emanuel Aracena
+ * Author:                  Emanuel Aracena Beriguete
  * Date:                    May 27, 2019
  * Project (working) title: MOS 6502 Emulator
  * Filename:                cpu.hpp
