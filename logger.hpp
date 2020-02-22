a2748d6f441afe2db548e1d3eeb200c09cc245e5
earacena <30157858+earacena@users.noreply.github.com>
2020-02-22 16:36:33 -0500

    Update logger.hpp

--- logger.hpp
+++ logger.hpp
@@ -1,5 +1,5 @@
 /*
- * Author:                  Emanuel Aracena
+ * Author:                  Emanuel Aracena Beriguete
  * Date Created:            June 3, 2019
  * Project (working) title: MOS 6502 Emulator
  * Filename:                logger.hpp
